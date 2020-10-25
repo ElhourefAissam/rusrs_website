45f44aa7f5a1eb01f04b1f6dbe415e6698709b8c
ElhourefAissam <aissamelhouref@gmail.com>
2020-10-26 02:24:18 +0800

    Commit add route

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\API\ArticaleController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('Article', 'Api\ArticleController');
-Route::resource('Event', 'Api\EventController');
+Route::apiResource('Article', ArticaleController::class);
+Route::apiResource('Event', API\EventController::class);
+
+
